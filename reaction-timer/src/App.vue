b601063b0503eb241dc988ccaca4d34e60973781
Renato Regalado <renato.regalado@gmail.com>
2025-02-27 16:07:45 -0600

    created block

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <h1>Ren's Reaction Timer</h1>
+  <div>
+    <h1>Ren's Reaction Timer</h1>
+    <p>Test your reaction time!</p>
+    <button @click="startGame" :disabled="isPlaying">Start</button>
+    <Block v-if="isPlaying" :delay="delay" />
+  </div>
 </template>
 
 <script>
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
   components: {
+    Block
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    startGame() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+    }
   }
 }
 </script>
